c3f37b173d2764a78f6aac4401b7f3f26b145429
yannlahess <yannisbaggen@gmail.com>
2024-12-15 18:00:23 +0100

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+module.exports = {
+  content: [],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
