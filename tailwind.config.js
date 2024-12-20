3bd64618de186d564c1d1329f0ecf296619adbe3
yannlahess <yannisbaggen@gmail.com>
2024-12-20 14:56:17 +0100

    ready for deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
